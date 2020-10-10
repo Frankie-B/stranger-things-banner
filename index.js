8376b9171a3664c3b01287a2b6a1dc28aadf36df
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-10-10 22:30:51 +0200

    Add anim to cta

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const canvas = document.querySelector('#stage');
 let tl = gsap.timeline();
 
 tl.to('.banner-img', { duration: 3, scale: 0.5 })
+tl.fromTo('.cta-wrapper',  {x: 0, opacity:0}, {duration: 2.5, x: 140, opacity:1, ease: "power4.out", display: 'flex'}, "=-0.5" ) 
+
+
+  // start.add(TweenMax.fromTo( $(".reveal") , 1 ,{opacity:0}, {opacity:1, ease: Expo.easeInOut} ), "=-0.5") ;
